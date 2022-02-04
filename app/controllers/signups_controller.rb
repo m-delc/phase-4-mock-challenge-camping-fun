0464b9f5bffb946e344f6fed37bdce1b32202d98
Michael Delcambre <delcambrem@gmail.com>
2022-02-03 21:15:46 -0600

    2115

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,16 @@
 class SignupsController < ApplicationController
+
+    def create
+        signup = Signup.create!(signup_params)
+        render json: signup.activity, status: 201
+    rescue ActiveRecord::RecordInvalid => e
+        render json: { errors: e.record.errors.full_messages }, status: 422
+    end
+
+    private
+
+    def signup_params
+        params.permit(:time, :camper_id, :activity_id)
+    end
+
 end
